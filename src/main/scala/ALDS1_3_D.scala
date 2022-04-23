addfd4b7f7e1007c1f5ea21afa72c4516b68cac7
Ken Kuwata <ken@farmx.co>
2022-04-22 18:03:53 -0700

    Assignment

---
+++ src/main/scala/ALDS1_3_D.scala
@@ -0,0 +1,16 @@
+
+import scala.io.StdIn
+
+object ALDS1_3_D {
+
+  class Lakes {
+    case class Lake
+  }
+
+  def main(args: Array[String]): Unit = {
+    val n = StdIn.readInt()
+
+    val lineArray = StdIn.readLine().split("")
+
+  }
+}
